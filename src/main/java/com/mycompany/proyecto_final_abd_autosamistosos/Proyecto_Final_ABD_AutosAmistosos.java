38a38f05e60343b78db17ae8df5232f5da7f97c8
eduar <eduardo.dc@tecjerez.edu.mx>
2026-04-14 22:32:33 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/proyecto_final_abd_autosamistosos/Proyecto_Final_ABD_AutosAmistosos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final_abd_autosamistosos;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Proyecto_Final_ABD_AutosAmistosos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
